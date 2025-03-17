c1b509df70b05313e80f7f657c9d2e35103fb111
Sai Suhas Sangaraju <136716323+saisuhas432005@users.noreply.github.com>
2025-03-17 06:26:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ let board = ["", "", "", "", "", "", "", "", ""];
 let currentPlayer = "X";
 let running = true;
 
-// Winning Combinations
 const winConditions = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8], 
     [0, 3, 6], [1, 4, 7], [2, 5, 8], 
